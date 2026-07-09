49488a697881184bff68f9eee53b0d9b399b0de6
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-09 14:30:28 -0400

    Add `SQLiteDatabaseError`

---
+++ Sources/CoreModelSQLite/Error.swift
@@ -0,0 +1,23 @@
+//
+//  Error.swift
+//  CoreModel-SQLite
+//
+//  Created by Alsey Coleman Miller on 7/9/25.
+//
+
+import Foundation
+import CoreModel
+@preconcurrency import SQLite
+
+/// CoreModel SQLite Error
+public enum SQLiteDatabaseError: Error {
+
+    /// A value read from the database could not be decoded as the declared attribute type.
+    case invalidBinding(Binding?, AttributeType)
+
+    /// Unknown property for the entity.
+    case invalidProperty(PropertyKey, EntityName)
+
+    /// The predicate cannot be represented as SQL.
+    case invalidPredicate(FetchRequest.Predicate)
+}
